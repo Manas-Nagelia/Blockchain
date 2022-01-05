373d16e6c0bc30a3974210020be6db2bf76258e4
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-01-04 20:59:33 -0500

    Created basic Block class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello");
+class Block {
+  private data: JSON;
+  private hash: String;
+  private previousHash: String;
+  private timestamp : Date;
+  private proofOfWork = 0;
+
+  constructor(previousHash: String, data: JSON) {
+    this.data = data;
+    this.hash = this.calculateHash();
+    this.previousHash = previousHash;
+    this.timestamp = new Date();
+    this.proofOfWork = 0;
+  }
+}
