5fe88b5d8227ee0f716f25b82fc765df24ead828
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-01-07 22:49:04 -0500

    Created Blockchain class

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,74 @@
 import { SHA256 as hash } from "crypto-js";
 
+/**
+ * Creates a new block in the blockchain
+ */
 class Block {
   private data: JSON;
-  private hash: string;
-  private previousHash: string;
+  private _hash: string;
+  private _previousHash: string;
   private timestamp: Date;
   private proofOfWork = 0;
-
+  /**
+   * @param  {string} previousHash - Hash of the previous block. Set to 0 if this is the first block.
+   * @param  {JSON} data - Data to be stored in the block
+   */
   constructor(previousHash: string, data: JSON) {
     this.data = data;
-    this.hash = this.calculateHash();
-    this.previousHash = previousHash;
+    this._hash = this.calculateHash();
+    this._previousHash = previousHash;
     this.timestamp = new Date();
     this.proofOfWork = 0;
   }
 
-  calculateHash(): string {
+  get previousHash() {
+    return this._previousHash;
+  }
+
+  get hash() {
+    return this._hash;
+  }
+
+  calculateHash() {
     return hash(
-      this.previousHash +
+      this._previousHash +
         JSON.stringify(this.data) +
         this.timestamp +
         this.proofOfWork
     ).toString();
   }
 
-  mine(difficulty: number): void {
-    while (!this.hash.startsWith("0".repeat(difficulty))) {
+  mine(difficulty: number) {
+    while (!this._hash.startsWith("0".repeat(difficulty))) {
       this.proofOfWork++;
-      this.hash = this.calculateHash();
+      this._hash = this.calculateHash();
+    }
+  }
+}
+
+class Blockchain {
+  private chain: Block[];
+
+  constructor() {
+    let data = "{ isGenesis : true }";
+    let genesisBlock = new Block("0", JSON.parse(data));
+    this.chain = [genesisBlock];
+  }
+
+  addBlock(data: JSON) {
+    let lastBlock = this.chain[this.chain.length - 1];
+    let newBlock = new Block(lastBlock.hash, data);
+    newBlock.mine(2); // Finds the hash of the new block
+    this.chain.push(newBlock);
+  }
+
+  isValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash != currentBlock.calculateHash()) return false;
+      if (currentBlock.previousHash != previousBlock.hash) return false;
     }
   }
 }
