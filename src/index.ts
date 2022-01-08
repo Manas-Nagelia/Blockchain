8339c90ab0cbdcf571d8e389c36845aa199a549f
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-01-07 22:26:27 -0500

    Replaced data types with primitive types

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { SHA256 as hash } from "crypto-js";
 
 class Block {
   private data: JSON;
-  private hash: String;
-  private previousHash: String;
+  private hash: string;
+  private previousHash: string;
   private timestamp: Date;
   // How much time does it take to mine a block?
   private proofOfWork = 0;
 
-  constructor(previousHash: String, data: JSON) {
+  constructor(previousHash: string, data: JSON) {
     this.data = data;
     this.hash = this.calculateHash();
     this.previousHash = previousHash;
@@ -17,9 +17,9 @@ class Block {
   }
   /**
    * Calculates the hash of the block
-   * @returns {String} - The hash of the block
+   * @returns {string} - The hash of the block
    */
-  calculateHash(): String {
+  calculateHash(): string {
     return hash(
       this.previousHash +
         JSON.stringify(this.data) +
@@ -28,9 +28,9 @@ class Block {
     ).toString();
   }
   /**
-   * @param  {Number} difficulty - Represents the difficulty of mining a block. The higher the difficulty, the longer it takes to mine a block.
+   * @param  {number} difficulty - Represents the difficulty of mining a block. The higher the difficulty, the longer it takes to mine a block.
    */
-  mine(difficulty: Number): void {
+  mine(difficulty: number): void {
     while (!this.hash.startsWith("0".repeat(difficulty))) {
       this.proofOfWork++;
       this.hash = this.calculateHash();
