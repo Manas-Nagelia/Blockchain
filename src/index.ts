5b3577124feca23407fd812e79da2fa231f7fd20
Manas Nagelia <84251015+undevable@users.noreply.github.com>
2022-01-12 18:26:56 -0500

    Added implementation example

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
 import { SHA256 as hash } from "crypto-js";
+import "colors";
+
+interface GenesisBlock {
+  isGenesis: boolean;
+}
+
+interface BlockData {
+  from: string;
+  to: string;
+  amount: number;
+}
 
 /**
  * Creates a new block in the blockchain
  */
 class Block {
-  private data: JSON;
+  public data: BlockData | GenesisBlock;
   private _hash: string;
   private _previousHash: string;
   private timestamp: Date;
   private proofOfWork = 0;
   /**
    * @param  {string} previousHash - Hash of the previous block. Set to 0 if this is the first block.
-   * @param  {JSON} data - Data to be stored in the block
+   * @param  {BlockData | GenesisBlock} data - Data to be stored in the block
    */
-  constructor(previousHash: string, data: JSON) {
+  constructor(previousHash: string, data: BlockData | GenesisBlock) {
     this.data = data;
     this._hash = this.calculateHash();
     this._previousHash = previousHash;
@@ -47,28 +58,60 @@ class Block {
 }
 
 class Blockchain {
-  private chain: Block[];
+  private _chain: Block[];
 
   constructor() {
-    let data = "{ isGenesis : true }";
-    let genesisBlock = new Block("0", JSON.parse(data));
-    this.chain = [genesisBlock];
+    let data: GenesisBlock = { isGenesis: true };
+    let genesisBlock = new Block("0", data);
+    this._chain = [genesisBlock];
   }
 
-  addBlock(data: JSON) {
-    let lastBlock = this.chain[this.chain.length - 1];
+  get chain() {
+    return this._chain;
+  }
+
+  addBlock(data: BlockData) {
+    let lastBlock = this._chain[this._chain.length - 1];
     let newBlock = new Block(lastBlock.hash, data);
-    newBlock.mine(2); // Finds the hash of the new block
-    this.chain.push(newBlock);
+    newBlock.mine(6); // Finds the hash of the new block
+    this._chain.push(newBlock);
   }
 
   isValid() {
-    for (let i = 1; i < this.chain.length; i++) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
+    for (let i = 1; i < this._chain.length; i++) {
+      const currentBlock = this._chain[i];
+      const previousBlock = this._chain[i - 1];
 
       if (currentBlock.hash != currentBlock.calculateHash()) return false;
       if (currentBlock.previousHash != previousBlock.hash) return false;
     }
+    return true;
   }
+}
+
+let blockchain = new Blockchain();
+
+let blockOne: BlockData = {
+  from: "Viet",
+  to: "David",
+  amount: 100,
+};
+
+let blockTwo: BlockData = {
+  from: "Adam",
+  to: "Beck",
+  amount: 150,
+};
+
+blockchain.addBlock(blockOne);
+blockchain.addBlock(blockTwo);
+let davidData: GenesisBlock | BlockData = blockchain.chain[1].data;
+if ("amount" in davidData) davidData.amount += 200;
+console.log(blockchain.chain);
+
+let isValid = blockchain.isValid();
+if (isValid) {
+  console.log("Blockchain is valid".green);
+} else {
+  console.log("Blockchain is invalid".red);
 }
